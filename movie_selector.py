17405408ed33b58ef349f4b280b8394843e8d7a1
TomDidier <45769051+TomDidier@users.noreply.github.com>
2018-12-11 13:50:58 +0100

    Add files via upload

---
+++ movie_selector.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import ast
+from tabulate import tabulate
+from flask import Flask, send_from_directory, render_template
+app = Flask(__name__)
+
+movies = pd.read_csv("Projet de groupe/tmdb_5000_movies.csv", delimiter= ",")
+
+movies['genres'] = [list(set([y['name'] for y in x])) for x in movies['genres'].apply(ast.literal_eval)] #converting the columns that are into json format into a simple list thanks to library ast
+
+@app.route('/')
+def mainpage():
+    return render_template("selectmoviebis.html", newChoice=True, genres={}, selectedfilm=False) #defining the web interface function
+
+@app.route('/choice/<genres_choisis>')
+def choixfilm(genres_choisis):
+    global movies #calling the dataframe we created out of the function
+    genresdico = {"TV Movie":genres_choisis=="TVMovie",
+    "Comedy":genres_choisis=="Comedy",
+    "Thriller":genres_choisis=="Thriller",
+    "War":genres_choisis=="War",
+    "Foreign":genres_choisis=="Foreign",
+    "Music":genres_choisis=="Music",
+    "Animation":genres_choisis=="Animation",
+    "Documentary":genres_choisis=="Documentary",
+    "Action":genres_choisis=="Action",
+    "Science Fiction":genres_choisis=="ScienceFiction",
+    "Drama":genres_choisis=="Drama",
+    "Adventure":genres_choisis=="Adventure",
+    "Mystery":genres_choisis=="Mystery",
+    "Western":genres_choisis=="Western",
+    "History":genres_choisis=="History",
+    "Fantasy":genres_choisis=="Fantasy",
+    "Crime":genres_choisis=="Crime",
+    "Family":genres_choisis=="Family",
+    "Horror":genres_choisis=="Horror",
+    "Romance":genres_choisis=="Romance"} #defining a dictionnary with "True" for the genre we chose, so that the application understands what genre we chose (for the HTML)
+    genres_choisis=[genres_choisis] #transforming the genre we chose (a string) into a list so that 
+    df2 = movies[[all(x in y for x in genres_choisis) for y in movies["genres"].values]] #this function works (it selects the movies in this genre from the dataframe)
+    df2 = df2.sort_values('vote_average', ascending=False) #rank movies according to their average rating
+    Filmschoisis1=df2['title'].head(10).values.tolist() #select the ten first ones
+    Filmschoisis2=", \n".join(Filmschoisis1) #transform the list of movies into a string (the format of selectedfilm)
+    return render_template("selectmoviebis.html", newChoice=False, genres=genresdico, selectedfilm=Filmschoisis2) #sending the chosen genres and the selected movies to the html
+
+@app.route('/img/<nom_image>')
+def sendImages(nom_image):
+    return send_from_directory('static/img', nom_image) #sending the images to the CSS code
+
+@app.route('/style.css')
+def sendCSS():
+    return send_from_directory('static','style.css') #defining the function for the CSS format
+
+if __name__ == '__main__':
+   app.run() #running the web application
---
+++ top10.py
@@ -0,0 +1,122 @@
+#Advise: run in Jupyter notebook for the HTML library to work
+
+import pandas as pd
+import ast
+import re
+from tabulate import tabulate
+from IPython.display import Image, HTML
+
+movies = pd.read_csv("Groupwork/tmdb_5000_movies.csv", delimiter= ",")
+allmovies = pd.read_csv("Groupwork/movies_metadata.csv")
+
+#####################
+#                   #
+# CLEANING          #
+#                   #
+#####################
+
+#converting the columns that are into json format into a simple list thanks to library ast
+movies['genres'] = [list(set([y['name'] for y in x])) for x in movies['genres'].apply(ast.literal_eval)]
+movies['id'] = movies['id'].astype('float')
+allmovies = allmovies[~allmovies.id.str.contains("-")] #drop rows where wrogn id format (like date!)
+allmovies['id'] = allmovies['id'].astype('float')
+movies = pd.merge(movies, allmovies[['id','poster_path']],on='id', how='left')
+
+#Get list of all the genres possible for a movi
+genre_possible = set(x for l in movies["genres"] for x in l)
+#print(genre_possible)
+
+#Create a column "decade" on your dataframe
+movies["release_date"] = pd.to_datetime(movies["release_date"])
+movies['year'] = movies['release_date'].dt.year
+def release_decade(c):
+    if c["year"] >1999 and c["year"] < 2010:
+        return 2000
+    elif c["year"] >= 2010:
+        return 2010
+    else:
+        return (c["year"] - ((c["year"]//100)*100))//10*10
+
+#Be careful: run the command step by step to avoid errors
+movies.loc[movies['decade'].isnull()] #see where value is na, only one movie is affected
+movies["decade"].fillna("2010", inplace=True)
+#movies.loc[movies['decade'].isnull()] #empty df so there is no longer a problem
+movies['decade'] = movies.apply(release_decade, axis=1)
+
+#####################
+#                   #
+# MODEL             #
+#                   #
+#####################
+
+#Loop to get what genres the user is interested in
+genres_choisi=[]
+while True:    # infinite loop
+    try:
+        element = input("What movie genre interests you (Press Q to stop)? ")
+        if element.upper() == "Q":
+            break  # stops the loop
+        elif element.lower().capitalize() in genres_choisi: #remove duplicates
+            print("This genre is already specified!")
+            pass
+        elif element.lower().capitalize() not in genre_possible: #Check if a chosen genre does exist in our database, if not remove it from our list and ask the user to input a new one or no
+            print(element, " is not a valid genre, please choose from", *genre_possible)
+            ajouter = input("Do you want to input a new genre? (Y/N) ")
+            if ajouter.upper() == "Y":
+                genres_choisi.append(input("What genre exactly?").lower().capitalize())
+            else:
+                break
+        else:
+            genres_choisi.append(element.lower().capitalize())
+    finally:
+        print("You're interested in ", *genres_choisi, sep = "\n")
+    if len(genres_choisi) > 5:
+        print("Too many genres! We'll keep", *genres_choisi)
+        break
+
+#Loop to get what decades the user is interested in
+decade_choisi = []
+while True:    # infinite loop
+    try:
+        element = int(input("Which decade interests you (Press 0 to stop)? "))
+        if element == 0:
+            break  # stops the loop
+        elif element in decade_choisi: #remove duplicates
+            print("This decade is already specified!")
+            pass
+        else:
+            decade_choisi.append(element)
+    except ValueError:
+        print("Input a number (10,20,..., 2000, 2010) please!")
+    finally:
+        print("You're interested in movies from era(s):", *decade_choisi, sep = "\n")
+    if len(decade_choisi) > 5:
+        print("You've already chosen 5 decades! We'll keep", *decade_choisi)
+        break
+
+decade_choisi = [float(x) for x in decade_choisi]
+
+#get the poster image
+base_poster_url = 'http://image.tmdb.org/t/p/w185/'
+movies['poster_path'] = "<img src='" + base_poster_url + movies['poster_path'] + "' style='height:100px;'>"
+
+#Dataframe with only movies in selected decade
+df1 = movies[movies['decade'].isin(decade_choisi)]
+
+#Générer une dataframe correspondant aux genres voulues 
+df2 = df1[[all(x in y for x in genres_choisi) for y in df1["genres"].values]]
+
+#Sort movies based on score calculated above and print a beautiful table of the TOP10
+df2 = df2.sort_values('vote_average', ascending=False)
+
+print("Out of", str(len(df1.index)), "movies meeting your criteria, here's the top 10..." )
+print(tabulate(df2[['title', "year", 'vote_average', 'genres']].head(10), headers=["Movie", "Year","Average score", "Genre"], showindex="never", tablefmt="fancy_grid"))
+
+gross_top = df2[['poster_path', 'title', 'vote_average']].head(10)
+HTML(gross_top.to_html(escape=False))
+
+#Parfois les images ne s'affichent pas, réessayez. 
+#df2.columns = ["Poster", "Movie title","Average score", "Genre"]
+df2 = df2[["poster_path","title", "vote_average", "genres"]].head(10)
+print(df2)
+display(HTML(df2.to_html(escape = False, index = False)))
